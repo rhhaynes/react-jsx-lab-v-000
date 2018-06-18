4b8e5b7fd2ca76c95c278b7a8617ac5f2e8a08b5
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-18 13:28:28 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <input/>
+        <input/>
+        <button></button>
+      </form>
+    );
+  }
+}
+
+export default RegistrationForm;
